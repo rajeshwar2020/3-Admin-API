068dd2822478ccfcb251da5fd525d43d3d771168
Rajeshwar Reddy <rajesh.chanty@gmail.com>
2024-10-09 17:08:41 -0400

    Add CrossOrigin support for this API

--- src/main/java/com/jrtp/controller/AdminController.java
+++ src/main/java/com/jrtp/controller/AdminController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:4200/")
 @RestController
 @RequestMapping(value = "/api/admin")
 public class AdminController {
