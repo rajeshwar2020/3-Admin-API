e7066af4eb1082a2f37903617c5dc0a5e851e823
Rajeshwar Reddy <rajesh.chanty@gmail.com>
2024-10-09 21:33:35 -0400

    modified SQL query

--- src/main/java/com/jrtp/repos/OrderRepo.java
+++ src/main/java/com/jrtp/repos/OrderRepo.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepo extends JpaRepository<Order, Integer>, JpaSpecificationExecutor<Order> {
 
-    @Query(value = "SELECT SUM(total_price) FROM orders", nativeQuery = true)
+    @Query(value = "SELECT SUM(total_price) FROM order_table", nativeQuery = true)
     public Double findTotalAmount();
 }
